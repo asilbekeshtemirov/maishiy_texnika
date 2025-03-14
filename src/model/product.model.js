95b7ca293fb7b199e2720f0836c36f588f44a064
Asilbek <asilbekeshtemirov@outlook.com>
2025-03-14 16:48:19 +0500

    Initial commit

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -1,3 +1,8 @@
 const productModel = `
-    CREATE DATABASE IF NOT EXISTS maishiy_texnika;
+    CREATE TABLE IF NOT EXISTS products;
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price NUMERIC(10,2) NOT NULL,
+        count SMALLINT NOT NULL DEFAULT 1 
+`
 `
